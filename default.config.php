9fb3a237e2ed37648fbc64c2b38cdf2280216620
Anne-Dot <ruusmann@gmail.com>
2024-10-23 11:49:57 +0300

    23.okt tunni uuendused

---
+++ default.config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $host = '';
+    $db   = '';
+    $user = '';
+    $pass = '';
+    $charset = 'utf8mb4';
+
+?>
--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
 
-$host = 'd123460.mysql.zonevs.eu';
-$db   = 'd123460_booksdbandrusen2idis';
-$user = 'd123460_kasutaja';
-$pass = 'T3re_raamatupood';
-$charset = 'utf8mb4';
+require_once('./connection.php');
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, $user, $pass, $options);
+$stmt = $pdo->query('SELECT id, title FROM books');
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<ol>
+    <?php while ( $row = $stmt->fetch() ) { ?>
+            <li>
+                <a href="./book.php?id=<?= $row['id']; ?>">
+                    <?= $row['title']; ?></li>
+                </a>
+    <?php } ?>
+</ol>
+
+</body>
+</html>
